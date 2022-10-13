df3a0ab57ac7d5c20761661efc843d73118eede1
camillehartt <115736977+camillehartt@users.noreply.github.com>
2022-10-13 14:48:07 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).foundation();
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
